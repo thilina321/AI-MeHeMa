0d8d347b84b8cc68eb84ce1aab26f0fe47487b31
MadhushanES <madhushan.es.pilapitiya@gmail.com>
2023-11-04 18:06:58 +0530

    Sinhala Q&A update v1.1
    Added 5 Questions and 20 Answers

--- lib/chatbot/lds10/chatbot.dart
+++ lib/chatbot/lds10/chatbot.dart
@@ -16,12 +16,12 @@ class _ChatBotState extends State<ChatBot> {
     "wkd.;h ms<sn|j Tnf.a n,dfmdfrd;a;=j flfiao@",//Anagathaya
     "Tn mrdcs;fhl= fia yef`.ao@", //Parajithaya
     "Tn lrk jev lghq;= .ek Tng oefkkafka flfiao@",//Wadakatayuthu
-    "Question 5: What is your favorite song?",
-    "Question 6: What is your favorite book?",
-    "Question 7: What is your favorite sport?",
-    "Question 8: What is your favorite game?",
-    "Question 9: What is your favorite TV show?",
-    "Question 10: What is your favorite hobby?",
+    "Tng jroldrS ye`.Sula oefka o@",//Waradakaraya
+    "Tn o`vqjula úÈk njla yef`.a o@",//Dandanaya
+    "Tng Tn .ek we;s u;h@",//Obe Mathaya
+    "Tng cSj;a úu ms<sn`oj yef`.kafka flfiao@",//SiyaDivi_nasa_Ganeema
+    "Tn y`vkjdo@",//Farcry_3
+    "Tnf.a fmkqu flfia o@",//Kathai_Monster
   ];
 
   List<List<String>> options = [
@@ -29,12 +29,12 @@ class _ChatBotState extends State<ChatBot> {
     ["ud yg wkd.;h ms<sn|j jeä n,dfmdfrd;a;=lv jQ .;shla ke;'", "ud yg wkd.;h ms<sn|j jeä n,dfmdfrd;a;=lv jQ .;shla we;'", "ud yg wkd.;fha i;=gq ùug lsisjla ke;s nj yef.a'", "ud yg wkd.;h b;d wÿre nj;a th lsisfia;au fydo w;g yeßug fkdyels nj;a yef.a'"], //Anagathaya
     ["uu mrð;fhla hehs fkdyef.a'", "uu idudkah wfhl=g jv mrð;fhla hehs  yef.a'", "w;S;h foi yÍ n,k l, uq¿ ðú;h mqrdu mrdð;h biau;= ù we;'", "uu mQ¾K mrð;fhl= hhs yef.a'"],//Parajithaya
     ["uu lrk lghq;= j,sk ,nk i;=g fmr mßÈu fõ'", "uu lrk lghq;= j,ska ,nk i;=g fmr mßäu fkd,efí'", "uu lrk lsisu fohlska kshu i;=gla ;j ÿrg;a fkd,efí'", "ug iEu fohlau fkdi;=gg fyda tmdùu f.k foa'"],//Wadakatayuthu
-    ["Shape of You", "Believer", "Faded", "Perfect"],
-    ["Harry Potter", "Twilight", "Hunger Games", "Divergent"],
-    ["Cricket", "Football", "Basketball", "Tennis"],
-    ["PUBG", "Fortnite", "Free Fire", "Call of Duty"],
-    ["Friends", "Game of Thrones", "Breaking Bad", "Money Heist"],
-    ["Reading", "Writing", "Singing", "Dancing"],
+    ["uu je/oaola l<d hehs ye.Sula ug we;s fkdfõ'", "uu je/oaola l<d hehs ye.Sula ug bo ysg w;s fõ'", "iEfykak jrola l<e hehs ye.Sula ug fndfyd fõ,djg we;s fõ'", "jrola  l<d hehs lshk ye.Su ug È.gu ;sfí'"],//Waradakaraya
+    ["uu ovqjula úÈk nj fkdyef.hs'", "ug ovqjula úÈug fõoehs ye.Sula we;s fõ'", "ug ovqjula ,efí hhs isf;a'", "uu ovqjula úÈñka isák nj yef.hs'"],//Dandanaya
+    ["ud yg ud .eku tmd jqk iajNdjhla ke;'", "ud yg ud .eku tmd jqk iajNdjhla we;'", "u yg ud .ek ;o n, tmd jQ iajNdjhla we;'", "uu ud yg ffjr lrñ'"],//Obe Mathaya
+    ["ug ishÈú kid .ekSug woyila ke;'", "ug ishÈú kid .ekSug woyila m, jqjo uu tAj l%sh;aul fkdlrñ'", "ug ishÈú kid .ekSug leu;shs'", "wjia:djla ,o fyd;a uu ishÈú kid .ksñ'"],//SiyaDivi_nasa_Ganeema
+    ["uu idudkafhka w`vk m%udkh jeä ù ke;'", "uu idudkafhka w`vk m%udkh jeä ù we;'", "uu ieu úgu wvñ'", "ug wvkakg lÿ¿ tka ke;'"],//FarCry_3
+    ["uf.a fmkqu fmrg jvd krl hhs fkdis;ñ'", "uf.a fmkqu is;a weo fkd.kakd nj is;ñ'", "ug uf.a fmkqfï is;a weo .kakd iq¿nj ke;s lrk iaÓr fjkialï we;s jk nj yefÕhs'", "uf.a fmkqu le; hhs uu úYajdi lrñ'"],//Kathai_Monster
   ];
 
   void nextQuestion() {
